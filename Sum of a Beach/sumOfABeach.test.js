d242b53acd191be4926027d548ee7d131e8b8f1c
squarepancakes <goh.shuyun@gmail.com>
2019-10-03 11:55:35 +0800

    edited test description

--- Sum of a Beach/sumOfABeach.test.js
+++ Sum of a Beach/sumOfABeach.test.js
@@ -3,41 +3,41 @@ const sumOfABeach = require('./sumOfABeach')
 describe("number of times, sun, water, fish and sand appear", () => {
    describe("lowercase", () => {
        
-       it("should return 1", () => {
+       it("should return 1 when string is water", () => {
            expect(sumOfABeach("water")).toBe(1)
         })
-        it("should return 2", () => {
+        it("should return 2 when string is sunwater", () => {
             expect(sumOfABeach("sunwater")).toBe(2)
         })
-        it("should return 3", () => {
+        it("should return 3 when string is sunwaterfish", () => {
             expect(sumOfABeach("sunwaterfish")).toBe(3);
         })
-        it("should return 4", () => {
+        it("should return 4 when string is sunwaterfishsand", () => {
             expect(sumOfABeach("sunwaterfishsand")).toBe(4);
         })
     })
 
     describe("uppercase", () => {
-        it("should return 1", () => {
+        it("should return 1 when string is WATER", () => {
             expect(sumOfABeach("WATER")).toBe(1)
         })
-        it("should return 2", () => {
+        it("should return 2 when string is SUNWATER", () => {
             expect(sumOfABeach("SUNWATER")).toBe(2)
         })
     })
 
-    describe("multiples of sun, water, sand and fish", () => {
-        it("should return 2", ()=> {
+    describe("when multiples of sun, water, sand and fish appear", () => {
+        it("should return 2 when string is waterwater", ()=> {
             expect(sumOfABeach("waterwater")).toBe(2);
         })
 
-        it("should return 3", ()=> {
+        it("should return 3 when string is waTerwatErSuN", ()=> {
             expect(sumOfABeach("waTerwatErSuN")).toBe(3);
         })
-        it("should return 1", () => {
+        it("should return 1 when string is WAtErSlIde", () => {
             expect(sumOfABeach("WAtErSlIde")).toBe(1);
         })
-        it("should return 3", () => {
+        it("should return 3 hen string is wGolDeNSanDyWateRyBeaChSuNN", () => {
             expect(sumOfABeach("GolDeNSanDyWateRyBeaChSuNN")).toBe(3);
         })
     })
