c9378880c1d9004c278c680b882800209909ff83
squarepancakes <goh.shuyun@gmail.com>
2019-10-21 20:46:21 +0800

    calculateCharges kata

--- parking-charges/calculateCharges.js
+++ parking-charges/calculateCharges.js
@@ -1,34 +1,34 @@
-const userAndTime = {A: 5, B: 9, C: 23}
-
 function calculateCharges(userAndTime) {
-	const person = Object.keys(userAndTime);
+	const users = Object.keys(userAndTime);
 	const parkingCharges = { User: "Fees" };
+	const minFees = 2.00;
+	const minHours = 3.00;
+	const hourlyFeeAfterFirst3Hours = 0.5;
+	const maxFees = 10.0;
+	const maxHours = (maxFees - minFees) / hourlyFeeAfterFirst3Hours + minHours;
 	let totalFees = 0;
 
-	person.forEach(onePerson => {
-		const time = userAndTime[onePerson];
-		if (time <= 3) {
-			totalFees += 2;
-			return (parkingCharges[onePerson] = "$2.00");
+	users.forEach(user => {
+		const time = userAndTime[user];
+
+		if (time <= minHours) {
+			totalFees += minFees;
+			return (parkingCharges[user] = `$${minFees.toFixed(2)}`);
 		}
-		
-		if (time > 19) {
-			totalFees += 10;
-			return (parkingCharges[onePerson] = "$10.00");
+		if (time > maxHours) {
+			totalFees += maxFees;
+			return (parkingCharges[user] = `$${maxFees.toFixed(2)}`);
 		}
 		const exceededTime = time - 3;
 		const fees = (2 + Math.ceil(exceededTime) * 0.5).toFixed(2);
 		totalFees += Number(fees);
-		return (parkingCharges[onePerson] = `$${fees}`);
+		return (parkingCharges[user] = `$${fees}`);
 	});
 	parkingCharges.Total = `$${totalFees.toFixed(2)}`;
 	Object.keys(parkingCharges).forEach(function(key) {
-		// console.log(key, parkingCharges[key]);
+		console.log(key, parkingCharges[key]);
 	});
 	return parkingCharges;
 }
-
-
-calculateCharges(userAndTime);
 
 module.exports = calculateCharges;
